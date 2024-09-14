ec283ea99016df6dbdb77cc4094a5129b2dfeee3
kthurman59 <kthurman59@gmail.com>
2024-09-14 16:24:21 -0500

    routes creation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('send-message', [ChatController::class, 'sendMessage']);
+
 
 require __DIR__.'/auth.php';
