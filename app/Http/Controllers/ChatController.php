4c02c2f310244b751e05773baf0c444b4557d451
kthurman59 <kthurman59@gmail.com>
2024-09-14 16:09:50 -0500

    ChatController created

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Events\ChatMessage;
+
+class ChatController extends Controller
+{
+    public function sendMessage(Request $request)
+    {
+        // Validate the request data (e.g. message content should not be empty)
+        $request->validate([
+            'message' => 'required|string',
+        ]);
+
+        // Trigger the ChatMessage event with sender's name and message content
+        event(new ChatMessage($request->user()->name, $request->message));
+
+        // Return a response indicating the message was sent
+        return response()->json(['message' => 'Message sent']);
+    }
+}
