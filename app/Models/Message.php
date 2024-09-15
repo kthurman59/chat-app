8af617233e74b23d96a73af608a0a38bb8cbdb81
kthurman59 <kthurman59@gmail.com>
2024-09-15 12:57:46 -0500

    message model updated

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -1,9 +1,18 @@
-public function user()
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
 {
-    return $this->belongsTo(User::class);
+    use HasFactory;
 }
 
-public function room()
-{
-    return $this->belongsTo(Room:class);
-}
+Schema::create('messages', function (Blueprint $table) {
+    $table->id();
+    $table->foreignId('user_id')->constrained()->onDelete('cascade');
+    $table->text('message_body');
+    $table->timestamps();
+});
