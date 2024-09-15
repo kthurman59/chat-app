ee7505ec84ec044c1c16aa37aeecbc6c3d497631
kthurman59 <kthurman59@gmail.com>
2024-09-15 10:08:41 -0500

    messagesent flow corrected

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -10,35 +10,15 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent
-{
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    /**
-     * Create a new event instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
-    }
-}
-
-
 class MessageSent implements ShouldBroadcast
 {
-    public $message;
+    usae Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+    * Create a new event instance
+    *
+    * @param $message;
+    */
 
     public function __construct($message)
     {
