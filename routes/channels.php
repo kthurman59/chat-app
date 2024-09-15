a3cb5de790666c4583d504fcdf6a6f94a14c7ae5
kthurman59 <kthurman59@gmail.com>
2024-09-15 08:16:59 -0500

    vite and db site configured

--- routes/channels.php
+++ routes/channels.php
@@ -1,3 +1,7 @@
 Broadcast::channel('chat.{roomId}', function ($user) {
     return ['id' => $user->id, 'name' => $user->name];
 });
+
+Broadcast::channel('private-chat.{userId}', function ($user, $userId) {
+    return (int) $user->id === (int) $userId;
+});
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,8 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::post('send-message', [ChatController::class, 'sendMessage']);
-
 
 require __DIR__.'/auth.php';
