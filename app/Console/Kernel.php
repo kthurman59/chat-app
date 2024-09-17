c08af323e25b3a2016c74c9f2fc7c649aa41bcdb
kthurman59 <kthurman59@gmail.com>
2024-09-17 08:08:36 -0500

    consolekernel updated

---
+++ app/Console/Kernel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    protected $commands = [];
+
+    protected function schedule(Schedule $schedule)
+    {
+        // Define your scheduled commnads here
+    }
+
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commnads');
+
+        require base_path('routes/console.php');
+    }
+}
